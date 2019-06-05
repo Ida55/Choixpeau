0e329b167344712d05bf3cd7ddee863c6bda971d
Akbar <akhan2023@yahoo.com>
2019-06-05 13:48:05 +0200

    Animation en js du message

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 
 document.getElementById('chapeau').addEventListener('click', function(){
     let les_maisons = ['400', '401', '403', '404'];
-
+    let msg = document.getElementById('msg');
     let maison = les_maisons[Math.floor(Math.random()*les_maisons.length)];
 
-    document.getElementById('msg').innerHTML = `Ta maison sera : ${maison}`;
-})
+    msg.innerHTML = `Ta maison sera : ${maison}`;
+
+    // Animation :
+    msg.style.left = '280px';
+    msg.style.top = '245px';
+
+    setTimeout(function(){
+        msg.style.top = '100px';
+        msg.style.left = '350px';
+        msg.style.opacity = '1';
+        msg.style.transform = 'scale(1)';
+    }, 20)
+
+});
+
+
+
+/* 
+
+image : 471px 371px
+x : 280px
+y : 245px
+
+*/
